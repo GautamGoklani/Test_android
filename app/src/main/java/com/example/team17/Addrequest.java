d225cc60f793e17823111198a1855c7fd58f309d
gauta <gautamgoklani135@gmail.com>
2022-09-25 17:45:02 +0530

    Team17

--- app/src/main/java/com/example/team17/Addrequest.java
+++ app/src/main/java/com/example/team17/Addrequest.java
@@ -3,7 +3,7 @@ package com.example.team17;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,21 +19,18 @@ import java.util.Objects;
 public class Addrequest extends AppCompatActivity {
 
     FirebaseAuth mAuth;
-    static int req_count=0;
-    FirebaseDatabase database;
-    DatabaseReference reference;
-    Button add_btn;
-    public TextInputEditText reg_title, reg_description, reg_features;
-    public TextInputLayout reg_title_label, reg_description_label, reg_features_label;
+    TextInputEditText reg_title, reg_description, reg_features;
+    TextInputLayout reg_title_label, reg_description_label, reg_features_label;
     Spinner spn_category;
+    static int req_count=0;
 
-    @SuppressLint("ResourceAsColor")
+    @SuppressLint({"ResourceAsColor", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addrequest);
         Objects.requireNonNull(getSupportActionBar()).hide();
-
+//
 //        reg_title = findViewById(R.id.pro_title);
 //        reg_description = findViewById(R.id.description);
 //        reg_features = findViewById(R.id.features);
@@ -42,31 +39,29 @@ public class Addrequest extends AppCompatActivity {
 //        reg_description_label = findViewById(R.id.reg_description);
 //        reg_features_label = findViewById(R.id.reg_features);
 
-        add_btn = findViewById(R.id.add_btn);
-
         mAuth = FirebaseAuth.getInstance();
+    }
 
-        add_btn.setOnClickListener(view -> {
-            if (!validatetitle() | validatedescription() | validatefeatures()) {
-                return;
-            } else {
-                String title = reg_title.getText().toString().trim();
-                String description = reg_description.getText().toString().trim();
-                String features = reg_features.getText().toString().trim();
-                String category = spn_category.getSelectedItem().toString().trim();
-                req_count++;
+    public void AddItem(View view) {
+        /*if(!validatetitle() | !validatedescription() | !validatefeatures()){
+            return;
+        }*/
+//        String title = reg_title.getText().toString().trim();
+//        String description = reg_description.getText().toString().trim();
+//        String features = reg_features.getText().toString().trim();
+//        String category = spn_category.getSelectedItem().toString().trim();
 
-                database = FirebaseDatabase.getInstance();
-                reference = database.getReference("Requests");
-                AddRequestClass addRequestClass = new AddRequestClass(title, description, category, features);
-                String userid = mAuth.getUid();
-                if (userid != null) {
-                    reference.child(userid).child(String.valueOf(req_count)).setValue(addRequestClass);
-                    startActivity(new Intent(Addrequest.this, MainActivity.class));
-                    finish();
-                }
-            }
-        });
+        String title="abc",description="abc",category="abc",features="abc";
+        req_count++;
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("Requests");
+        AddRequestClass addRequestClass = new AddRequestClass(title, description, category, features);
+        String userid = mAuth.getUid();
+        reference.child(userid).child(String.valueOf(req_count)).setValue(addRequestClass);
+//        reference.child(userid).child(String.valueOf(req_count)).setValue(addRequestClass);
+        startActivity(new Intent(Addrequest.this, MainActivity.class));
+        finish();
     }
 
     private boolean validatefeatures() {
@@ -74,9 +69,8 @@ public class Addrequest extends AppCompatActivity {
         if (val.isEmpty()) {
             reg_features_label.setError("Features cannot be empty");
             return false;
-        } else {
-            return true;
         }
+        return true;
     }
 
     private boolean validatedescription() {
@@ -84,9 +78,8 @@ public class Addrequest extends AppCompatActivity {
         if (val.isEmpty()) {
             reg_description_label.setError("Description cannot be empty");
             return false;
-        } else {
-            return true;
         }
+        return true;
     }
 
     private boolean validatetitle() {
@@ -94,8 +87,7 @@ public class Addrequest extends AppCompatActivity {
         if (val.isEmpty()) {
             reg_title_label.setError("Title cannot be empty");
             return false;
-        } else {
-            return true;
         }
+        return true;
     }
 }
